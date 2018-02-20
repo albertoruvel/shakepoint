70a62a3d1f2a2824d343feb5a39a029644776054
Alberto <arubalcaba@24hourfit.com>
2018-02-19 20:41:10 -0800

    Updated operation

--- src/main/java/com/shakepoint/web/controller/rest/ShopRestController.java
+++ src/main/java/com/shakepoint/web/controller/rest/ShopRestController.java
@@ -70,7 +70,7 @@ public class ShopRestController {
     }
 
     @RequestMapping(value = "/getAvailablePurchaseForMachine", method = RequestMethod.GET, produces = "applicaiton/json")
-    public AvailablePurchaseResponse getAvailablePurchaseForMachine(@RequestParam(value = "productId") String productId, @RequestParam(value = "machineiD") String machineId){
+    public AvailablePurchaseResponse getAvailablePurchaseForMachine(@RequestParam(value = "productId") String productId, @RequestParam(value = "machineId") String machineId){
         return shopFacade.getAvailablePurchaseForMachine(productId, machineId);
     }
 
